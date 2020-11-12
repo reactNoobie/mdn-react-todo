d615f7f9242b0afed83756e800c2dbc5f03c7d5f
reactNoobie <reactnoobie@gmail.com>
2020-11-12 23:46:23 +0600

    Implementation for editing and filtering tasks

--- src/App.js
+++ src/App.js
@@ -5,16 +5,25 @@ import FilterButton from './components/FilterButton';
 import Form from './components/Form';
 import Todo from './components/Todo';
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: task => !task.completed,
+  Completed: task => task.completed
+};
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState('All');
 
   function addTask(name) {
+    // console.log('addTask', name);
     const newTask = { id: 'todo-' + nanoid(), name: name, completed: false };
     setTasks([...tasks, newTask]);
   }
 
   function toggleTaskCompleted(id) {
-    console.log(tasks.find(task => task.id === id));
+    // console.log(tasks.find(task => task.id === id));
     const updatedTasks = tasks.map(task => {
       if (task.id === id) {
         return { ...task, completed: !task.completed };
@@ -29,7 +38,27 @@ function App(props) {
     setTasks(updatedTasks);
   }
 
-  const taskList = tasks.map(task =>
+  function editTask(id, newName) {
+    // console.log('editTask ', id, newName);
+    const editedTasks = tasks.map(task => {
+      if (task.id === id) {
+        return { ...task, name: newName };
+      }
+      return task;
+    });
+    setTasks(editedTasks);
+  }
+
+  const filterButtons = FILTER_NAMES.map(filterName => (
+    <FilterButton
+      key={filterName}
+      title={filterName}
+      setFilter={setFilter}
+      isPressed={filterName === filter}
+    />
+  ));
+
+  const taskList = tasks.filter(FILTER_MAP[filter]).map(task =>
     <Todo
       id={task.id}
       name={task.name}
@@ -37,9 +66,17 @@ function App(props) {
       key={task.id}
       toggleTaskCompleted={toggleTaskCompleted}
       deleteTask={deleteTask}
+      editTask={editTask}
     />
   );
 
+  // const numberOfTasks = tasks.reduce((accum, task) => {
+  //   // console.log('task -> ', task, 'accum -> ', accum);
+  //   if (!task.completed) {
+  //     return accum + 1;
+  //   }
+  //   return accum;
+  // }, 0);
   const numberOfTasks = taskList.length;
   const taskNoun = numberOfTasks > 1 ? 'tasks' : 'task';
 
@@ -48,9 +85,7 @@ function App(props) {
       <h1>TodoMatic</h1>
       <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+        {filterButtons}
       </div>
       <h2 id="list-heading">
         {numberOfTasks} {taskNoun} remaining.
--- src/components/FilterButton.js
+++ src/components/FilterButton.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 function FilterButton(props) {
     return (
-        <button type="button" className="btn toggle-btn" aria-pressed="true">
+        <button
+            type="button"
+            className="btn toggle-btn"
+            aria-pressed={props.isPressed}
+            onClick={() => props.setFilter(props.title)}
+        >
             <span className="visually-hidden">Show </span>
-            <span>All</span>
+            <span>{props.title}</span>
             <span className="visually-hidden"> Tasks</span>
         </button>
     );
