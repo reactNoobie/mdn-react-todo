e214eac395f96b9ce70c34eb449c98cf751e8b6b
reactNoobie <reactnoobie@gmail.com>
2020-11-12 18:35:32 +0600

    Implemented adding, toggling and removing tasks
    Created reusable components for Form, FilterButton and Todo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,67 @@
+import React, { useState } from 'react';
+import { nanoid } from 'nanoid';
+
+import FilterButton from './components/FilterButton';
+import Form from './components/Form';
+import Todo from './components/Todo';
+
 function App(props) {
-  console.log(props);
+  const [tasks, setTasks] = useState(props.tasks);
+
+  function addTask(name) {
+    const newTask = { id: 'todo-' + nanoid(), name: name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+
+  function toggleTaskCompleted(id) {
+    console.log(tasks.find(task => task.id === id));
+    const updatedTasks = tasks.map(task => {
+      if (task.id === id) {
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+
+  function deleteTask(id) {
+    const updatedTasks = tasks.filter(task => task.id !== id);
+    setTasks(updatedTasks);
+  }
+
+  const taskList = tasks.map(task =>
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
+    />
+  );
+
+  const numberOfTasks = taskList.length;
+  const taskNoun = numberOfTasks > 1 ? 'tasks' : 'task';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello {props.subject}!</p>
-      </header>
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <Form addTask={addTask} />
+      <div className="filters btn-group stack-exception">
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
+      </div>
+      <h2 id="list-heading">
+        {numberOfTasks} {taskNoun} remaining.
+      </h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+        {taskList}
+      </ul>
     </div>
   );
 }
